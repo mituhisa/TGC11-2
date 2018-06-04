8a8fefd62b161725508cf866f6dba22717818e3f
mitu <mituhisa0208@gmail.com>
2018-06-04 12:24:52 +0900

    ホームとタイトル完成(仮)

---
+++ New Unity Project/Assets/Script/Fade.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Fade : MonoBehaviour {
+
+    float alfa;
+    public GameObject yazirusi; 
+    float fadespeed = 0.01f;
+    float red, green, blue;
+
+	// Use this for initialization
+	void Start () {
+        red = GetComponent<Text>().color.r;
+        green = GetComponent<Text>().color.g;
+        blue = GetComponent<Text>().color.b;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        GetComponent<Text>().color = new Color(red, green, blue, alfa);
+        alfa += fadespeed;
+        if (alfa >= 1) yazirusi.SetActive(true);
+	}
+}
--- New Unity Project/Assets/Script/Title.cs
+++ New Unity Project/Assets/Script/Title.cs
@@ -4,17 +4,81 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Title : MonoBehaviour {
+    public enum Mode
+    {
+        Opning,
+        Title,
+        Menu,
+        Select,
+    };
+
+    public GameObject charactor;
+    public GameObject SMKT;
+    public GameObject yazirusi;
+    public Mode mode;
+    Vector3 titlePos;
 
 	// Use this for initialization
 	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (SceneManager.GetActiveScene().name == "Home")
         {
-            SceneManager.LoadScene("Home");
+            charactor.gameObject.SetActive(true);
+            mode = Mode.Title;
+        }
+        else
+        {
+            mode = Mode.Opning;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (mode)
+        {
+            case Mode.Opning:
+                SMKT.SetActive(true);
+                charactor.SetActive(true);
+                break;
+            case Mode.Title:
+                SMKT.SetActive(false);
+                break;
+            case Mode.Menu:
+                //矢印上下移動
+                if (Input.GetKeyDown(KeyCode.UpArrow))
+                {
+                    if(yazirusi.transform.position.y != -0.7f)
+                    {
+                        yazirusi.transform.position = new Vector3(-1.8f, -0.7f, 0);
+                    }
+                }
+                if (Input.GetKeyDown(KeyCode.DownArrow))
+                {
+                    if(yazirusi.transform.position.y != -1.9f)
+                    {
+                        yazirusi.transform.position = new Vector3(-1.7f, -1.9f, 0);
+                    }
+                }
+
+                //選択分岐
+                if(yazirusi.transform.position.y >= -0.7f)
+                {
+                    if (Input.GetKeyDown(KeyCode.Space) && charactor.transform.position.x > -4.5f)
+                    {
+                        mode = Mode.Select;
+                        charactor.GetComponent<charctor>().mode = charctor.Mode.Copy;
+                        charactor.GetComponent<charctor>().waitTime = 0;
+                        GameObject.Find("Line").GetComponent<Line>().senFlg = false;
+                    }
+                }
+                else
+                {
+                    if (Input.GetKeyDown(KeyCode.Space)) SceneManager.LoadScene("End");
+                }
+                break;
+            case Mode.Select:
+   
+                break;
         }
 	}
 }
